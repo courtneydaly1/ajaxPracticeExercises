f0a246bbc2d71df13a20de5d0372f54e1aba9ffa
courtneydaly1 <141876079+courtneydaly1@users.noreply.github.com>
2023-09-18 17:33:44 -0400

    update appending image

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -4,38 +4,35 @@ const $gifGrid = $("#gifRows");
 //use ajax to get gifs from data
 async function getSearchData(e) {
   e.preventDefault();
-  debugger;
   let searchedTerm = $searchInput.val();
-debugger;
+  debugger;
   const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
     params: {
       q: searchedTerm,
       api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym",
     },
   });
-makeGifLi(response.data.data)
+  appendGif(response.data.data);
 }
 
-// create li to put the gif images in gif container
-function makeGifLi(gifs) {
+// gif images in gif container
+function appendGif(gifs) {
   debugger;
   let numResults = gifs.length;
   const randomIdx = Math.floor(Math.random() * numResults);
-  const newLi = document.createElement("LI");
-  const $newGif = $("<img>", {
-    src: gifs[randomIdx].images.original.url,
-    class: "gif",
-  });
-  newLi.append($newGif);
-  document.getElementById("ulContainer").append(newLi);
+  const imageSrc = gifs[randomIdx].url;
+  const image = new Image();
+  image.src = imageSrc;
+
+  $("imageContainer").append(image);
 }
+
 //add event listener to the submit button
 const submitBtn = document.querySelector("#searchBtn");
 searchBtn.addEventListener("click", getSearchData);
 
-
 //add event listener to button delete gifs when clicked
 const deleteBtn = document.querySelector("#removeBtn");
 deleteBtn.addEventListener("click", function () {
-  $gifGrid.empty();
+  $('imageContainer').empty();
 });
