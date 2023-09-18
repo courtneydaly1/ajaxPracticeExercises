c799c6d4bbed260ebe3be64790d8b0f57810045b
courtneydaly1 <141876079+courtneydaly1@users.noreply.github.com>
2023-09-18 12:36:11 -0400

    add event listener to search button

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -3,19 +3,22 @@ const $gifGrid = $("#gifRows");
 
 //use ajax to get gifs from data
 async function getSearchData(e) {
-  // e.preventDefault();
+  e.preventDefault();
+  debugger;
   let searchedTerm = $searchInput.val();
-
+debugger;
   const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
     params: {
       q: searchedTerm,
       api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym",
     },
   });
+console.log(response)
 }
 
 // create li to put the gif images in gif container
 function makeGifLi(gifs) {
+  debugger;
   let numResults = response.data.length;
   const randomIdx = Math.floor(Math.random() * numResults);
   const newLi = document.createElement("LI");
@@ -25,6 +28,11 @@ function makeGifLi(gifs) {
   });
   newLi.append($newGif);
 }
+//add event listener to the submit button
+const submitBtn = document.querySelector("#searchBtn");
+searchBtn.addEventListener("click", getSearchData);
+
+
 //add event listener to button delete gifs when clicked
 const deleteBtn = document.querySelector("#removeBtn");
 deleteBtn.addEventListener("click", function () {
